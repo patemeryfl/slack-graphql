c399f2f699986ffd66062ecb98b70a5ed7a682ae
Pat <pat.emery21@gmail.com>
2018-05-25 20:05:48 -0400

    Project set-up/add dependancies

---
+++ src/server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { makeExecutableSchema } from 'graphql-tools';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import typeDefs from './schema';
+import resolvers from './resolvers';
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+const app = express();
+
+// bodyParser is needed just for POST.
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+app.listen(8080);
---
+++ src/server/resolvers.js
@@ -0,0 +1,6 @@
+/* eslint no-unused-vars: 0 */
+export default {
+  Query: {
+    hi: (parent, args, context, info) => 'Hi',
+  },
+};
